4dee0fd0593d5805e25d5c8873b320a9061cc533
kubster96 <jungiewiczjakub@gmail.com>
2018-07-22 21:48:37 +0200

    Initial commit

---
+++ src/main/java/mathandel/backend/MathandelBackendApplication.java
@@ -0,0 +1,12 @@
+package mathandel.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathandelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathandelBackendApplication.class, args);
+    }
+}
