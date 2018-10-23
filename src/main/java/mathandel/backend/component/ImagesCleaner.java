c7261c842f0d1142cfe15d8bb50e5a8883263582
Kubster96 <jungiewiczjakub@gmail.com>
2018-10-23 10:51:46 +0200

    fix try

--- src/main/java/mathandel/backend/component/ImagesCleaner.java
+++ src/main/java/mathandel/backend/component/ImagesCleaner.java
@@ -9,20 +9,32 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-//@Component
-public class ImagesCleaner {
+@Component
+public class ImagesCleaner implements ApplicationRunner {
 
-//    private static final Logger logger = LoggerFactory.getLogger(ImagesCleaner.class);
-//
-//    @Override
-//    public void run(ApplicationArguments args) {
-//        File images = new File(new File("").getAbsolutePath() + "\\src\\main\\resources\\images");
-//        try {
-//            FileUtils.cleanDirectory(images);
-//            logger.info("Server ready to go...");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private static final Logger logger = LoggerFactory.getLogger(ImagesCleaner.class);
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+        String path = new File("").getAbsolutePath() + "\\src\\main\\resources\\images";
+
+        if(Files.exists(Paths.get(path))) {
+            File images = new File(path);
+            try {
+                FileUtils.cleanDirectory(images);
+                logger.info("Server ready to go...");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            if (new File(path).mkdir()) {
+                logger.info("Server ready to go...");
+            }
+        }
+    }
 }
