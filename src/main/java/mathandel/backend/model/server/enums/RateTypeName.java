65adc8b6e403950dafbdc0c7c9bc109bfb21cef6
monikeu <wake.up.when.you.need.me@gmail.com>
2018-11-18 23:04:37 +0100

    [IMPLEMENTATION] Changed RateTypeNames

--- src/main/java/mathandel/backend/model/server/enums/RateTypeName.java
+++ src/main/java/mathandel/backend/model/server/enums/RateTypeName.java
@@ -1,6 +1,5 @@
 package mathandel.backend.model.server.enums;
 
 public enum RateTypeName {
-    //todo nie pamietamjak sie miały nazywac xd plus mozna nadac im wartosci i potem srednia bedzie latwo liczyc
-    NAME1, NAME2, NAME3, NAME4;
+    NEGATIVE, NEUTRAL, POSITIVE, NOT_RECEIVED
 }
